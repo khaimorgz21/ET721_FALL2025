96b1f247a07a10366b94fb43225deaa6614b86a3
khaimorgz21 <158099433+khaimorgz21@users.noreply.github.com>
2025-10-14 15:37:13 +0000

    read and write data

--- lab7_data/lab7_main.py
+++ lab7_data/lab7_main.py
@@ -1,9 +1,33 @@
 """
 Makhai Morgan
 Lab 7, accesing data in a file
-Sep 29, 2025
+Oct 14, 2025
 """
 from lab7_function import *
 
 testing()
 print("\n ---- example 1: reading file -----")
+read_data("phrases.txt")
+
+print("\n ---- example 2: reading specific portion of a file -----")
+read_up("phrases.txt")
+
+print("\n ---- example 3: reading specific portion using readline -----")
+read_readline("phrases.txt")
+
+print("\n ---- example 4: reading specific portion using readlines -----")
+read_all("phrases.txt")
+
+print("\n ---- example 5: loop through each line -----")
+read_each("phrases.txt")
+
+print("\n ---- example 6: creating a new file -----")
+new_file("Morgan.txt")
+
+print("\n ---- example 7: append data into an existing file -----")
+stamp_date("Morgan.txt")
+
+print("\n ---- EXERCISE -----")
+count_yahoo = email_read("user_email.txt","@yahoo")
+count_email = email_read("user_email.txt","@gmail")
+count_hotmail = email_read("user_email.txt","@hotmail")
