d43e9bbad0acd09280fe1073139455bba7e2a2cd
khaimorgz21 <158099433+khaimorgz21@users.noreply.github.com>
2025-10-29 23:54:38 -0400

    Add files via upload

---
+++ project2/testmain.py
@@ -0,0 +1,8 @@
+import unittest
+from main import Connect4 # import class 'Connect4'
+
+class TestConnect4(unittest.TestCase):
+    def setUp(self):
+        self.board1()
+    def test_board1(self):
+        self.assertEqual
